b3b9968eaa42c20b45d37a8941bd7d0163ed2eae
kcmki <mekkimohe@gmail.com>
2023-09-12 02:11:28 +0100

    first commit

---
+++ Zip.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+from zipfile import ZipFile
+import os
+from PIL import Image
+import re
+
+def decode_escape_sequences(match):
+    escape_sequence = match.group(0)
+    try:
+        decoded_character = chr(int(escape_sequence[2:], 16))
+        return decoded_character
+    except ValueError:
+        return escape_sequence
+    
+
+def get_all_file_paths(directory):
+  
+    # initializing empty file paths list
+    file_paths = []
+  
+    # crawling through directory and subdirectories
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            # join the two strings in order to form the full filepath.
+            filepath = os.path.join(root, filename)
+            file_paths.append(filepath)
+  
+    # returning all file paths
+    return file_paths        
+
+directory_path = "D:\_dev\AI\diffusion\SwordMaker\Data\packs"
+
+
+paths = get_all_file_paths(directory_path)
+i=1
+paths = [path for path in paths if path.endswith(".zip")]
+
+#try:
+
+for file_name in paths:
+    print(file_name)
+    try:
+        with ZipFile(file_name,'r') as z:
+            image = [file for file in z.namelist() if file.endswith("diamond_sword.png")]
+            string = re.sub(r'\\x[0-9a-fA-F]+', decode_escape_sequences, image[0])
+            image = z.extract(string)
+            with Image.open(image) as img:
+                img.save("D:\_dev\AI\diffusion\SwordMaker\Data\extracted\sw"+str(i)+".png")
+            i=i+1
+            z.close()
+    except Exception as e:
+        print(e)
+"""         with ZipFile(file_name,'r') as z:
+            try:
+                image = [file for file in z.namelist() if file.endswith("diamond_sword.png")]
+                string = re.sub(r'\\x[0-9a-fA-F]+', decode_escape_sequences, image[0])
+                image = z.extract(string)
+                with Image.open(image) as img:
+                    img.save("D:\_dev\AI\diffusion\SwordMaker\Data\extracted\sw"+str(i)+".png")
+
+            except Exception as e: 
+                print(e)
+                pass """
+'''except Exception as e:
+    print(e)
+    pass'''
---
+++ imageTransformation.py
@@ -0,0 +1,56 @@
+import numpy as np
+import cv2 
+import csv 
+import matplotlib.pyplot as plt
+
+import os
+import cv2
+
+# Directory where your image files are located
+image_dir = 'D:\_dev\AI\diffusion\SwordMaker\Data'
+
+# Get a list of all files in the directory
+all_files = os.listdir(image_dir)
+
+# Filter files that start with "sw" and end with an image extension (e.g., jpg, png)
+sw_files = [file for file in all_files if file.startswith('sw') and (file.endswith('.jpg') or file.endswith('.png'))]
+
+# Loop through the filtered files and read each image using cv2
+i=126
+
+white_color = np.array([255, 255, 255])
+
+for file_name in sw_files:
+    file_path = os.path.join(image_dir, file_name)
+    image = cv2.imread(file_path,cv2.IMREAD_UNCHANGED)
+
+    if image is not None:
+
+        # Do whatever you want with the image here
+        # For example, you can display it using cv2.imshow or perform some processing
+
+        image = cv2.resize(image, (16, 16))
+
+        # Create an alpha channel with all non-white pixels set to 255 (fully opaque)
+        alpha_channel = np.all(image[:, :, :3] != white_color, axis=2).astype(np.uint8) * 255
+        # Add the alpha channel to the image
+        if image.shape[2] == 3:
+            image = np.dstack((image, alpha_channel))
+
+        clr1 = np.array(image[1, 1][:3])
+        clr2 = np.array(image[0, 0][:3])
+
+        for y in range(image.shape[0]):
+            for x in range(image.shape[1]):
+                if np.array_equal(image[y, x][:3], clr2):  # Check if pixel is white
+                    image[y, x] = [255,255,255,255]  # Set alpha channel to 0
+                if np.array_equal(image[y, x][:3], clr1):  # Check if pixel is white
+                    image[y, x] = [255,255,255,255]  # Set alpha channel to 0
+                if all(image[y, x] == [0,0,0,0]):  # Check if pixel is white
+                    image[y, x] = [255,255,255,255]  # Set alpha channel to 0
+        cv2.imwrite("D:\_dev\AI\diffusion\SwordMaker\Data\data16\csw"+str(i)+".png", image)
+        i=i+1
+
+# Wait for a key press and then close all windows
+cv2.waitKey(0)
+cv2.destroyAllWindows()
